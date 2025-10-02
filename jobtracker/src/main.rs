19be57e5a442cdfaa106ce87e13a5bbee6c6d4f8
brianquach123 <brianquach123@gmail.com>
2025-10-02 14:20:56 -0400

    Use defaults in main()

--- jobtracker/src/main.rs
+++ jobtracker/src/main.rs
@@ -1,20 +1,13 @@
 use chrono::Utc;
-use eframe::egui::ViewportBuilder;
-use jobtracker_core::{JobApp, APP_NAME, WINDOW_HEIGHT, WINDOW_WIDTH};
+use jobtracker_core::{JobApp, APP_NAME};
 
 fn main() -> eframe::Result<()> {
-    let options = eframe::NativeOptions {
-        viewport: ViewportBuilder::default()
-            .with_inner_size([WINDOW_WIDTH, WINDOW_HEIGHT])
-            .with_resizable(true),
-        ..Default::default()
-    };
+    let options = eframe::NativeOptions::default();
 
     let mut job_app = JobApp {
         last_refresh: Utc::now(),
         ..Default::default()
     };
-
     job_app.store.load_from_file().unwrap();
     eframe::run_native(APP_NAME, options, Box::new(|_cc| Ok(Box::new(job_app))))
 }
